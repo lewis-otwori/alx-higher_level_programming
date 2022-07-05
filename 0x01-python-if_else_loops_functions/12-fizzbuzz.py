0147289e01a3959d8943cd055bf176d31a63a500
lewis-otwori <lewisotwori@gmail.com>
2022-07-05 12:17:19 -0700

    12-fizzbuzz.py project

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,9 +7,9 @@ def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0 and i % 5 == 0):
             print("%s%s" % (str1, str2), end=' ')
-         elif (i % 3 == 0):
+        elif (i % 3 == 0):
              print("%s" % (str1), end=' ')
-         elif (i % 5 == 0):
+        elif (i % 5 == 0):
              print("%s" % (str2), end=' ')
-         else:
+        else:
              print("%d" % (i), end=' ')
